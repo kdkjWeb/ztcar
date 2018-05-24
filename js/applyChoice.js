df7dc76712e817ec160f6ab1293f616f072064c7
yuqiuju0829 <451169158@qq.com>
2018-05-24 10:48:07 +0800

    no message

--- js/applyChoice.js
+++ js/applyChoice.js
@@ -1,4 +1,9 @@
-$("#job").select({
-  title: "选择经销商",
-  items: ["法官", "医生", "猎人", "学生", "记者", "其他",]
-});
+$(function(){
+  $("#job").select({
+    title: "选择经销商",
+    items: ["法官", "医生", "猎人", "学生", "记者", "其他",]
+  });
+  $("#btn1").on("click",function(){
+    window.location.href="apply.html"
+  })
+})
