1d9db67cc6f95d070dd91ec4d2e397e46f54a2ae
yuqiuju0829 <451169158@qq.com>
2018-05-24 22:37:04 +0800

    的

--- js/applyChoice.js
+++ js/applyChoice.js
@@ -3,6 +3,20 @@ $(function(){
     title: "选择经销商",
     items: ["法官", "医生", "猎人", "学生", "记者", "其他",]
   });
+
+
+
+
+
+  $("#produce").select({
+    title:"选择车贷分期类型",
+    items:["诚易贷","诚易贷","诚易贷","诚易贷"]
+  })
+  $("#num").select({
+    title:"选择贷款期数",
+    items:["12期","36期","48期","其他"]
+  })
+
   $("#btn1").on("click",function(){
     window.location.href="apply.html"
   })
