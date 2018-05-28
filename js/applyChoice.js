482470a6bdfccaf6c24375bd2a2af12a608104fa
ong-king <1318281415@qq.com>
2018-05-28 10:17:45 +0800

    下拉样式

--- js/applyChoice.js
+++ js/applyChoice.js
@@ -1,3 +1,4 @@
+//============================新旧车点击
 $('#newCar').click(function() {
 	$('#oldCar').removeClass('icon-danxuan').addClass('icon-danxuan2');
 	$(this).removeClass('icon-danxuan2').addClass('icon-danxuan');
@@ -8,37 +9,9 @@ $('#oldCar').click(function() {
 	$(this).removeClass('icon-danxuan2').addClass('icon-danxuan');
 })
 
-$('.iconBox').click(function() {
-	var _that = $(this);
-	if($('#distributor').attr('data-values') != undefined) {
-		if(_that.attr('box') != 'true') {
-			_that.removeClass('icon-danxuan2').addClass('icon-danxuan');
-			_that.attr('box', 'true');
-				if(_that.attr('Adata') != 'data') { //获取分期数据
-					_that.attr('Adata', 'data');
-					
-					var str = $('#distributor').attr('data-values');
-					var arr = str.split("?");
-					var a  = arr[0];
-					
-					query(a, _that.attr('num'));
-
-				}
-
-		} else {
-			$(this).removeClass('icon-danxuan').addClass('icon-danxuan2');
-			$(this).attr('box', 'false')
-		}
-	} else {
-		errAlert('提醒', '请先选择经销商')
-	}
-
-})
-
-//========================
+//======================== 获取经销商
 
 $(function() {
-
 	var data = {};
 	$.ajax({
 		url: path + "/smDealers/query",
@@ -68,7 +41,8 @@ $(function() {
 							$(this).attr('adata','');
 							$(this).attr('box','false');
 							$(this).removeClass('icon-danxuan').addClass('icon-danxuan2');
-						})
+						});
+						getTime();//获取分期时间
 					}
 				});
 				//=======================
@@ -83,53 +57,39 @@ $(function() {
 		}
 	});
 
-	//	==============贷款期限=============
+})
 
-	$.ajax({
-		url: path + "/deadlineCategory/query",
-		data: JSON.stringify(data),
-		dataType: "json",
-		contentType: "application/json",
-		type: "post",
-		success: function(data) {
-			if(data.code == 0) {
-				console.log(data)
+//=======产品前面的多选按钮点击事件============
+$('.iconBox').click(function() {   
+	var _that = $(this);
+	if($('#distributor').attr('data-values') != undefined) {
+		if(_that.attr('box') != 'true') {
+			_that.removeClass('icon-danxuan2').addClass('icon-danxuan');
+			_that.attr('box', 'true');
+				if(_that.attr('Adata') != 'data') { //获取分期数据
+					_that.attr('Adata', 'data');
+					var str = $('#distributor').attr('data-values');
+					var arr = str.split("?");
+					var a  = arr[0];
+					query(a, _that.attr('num'));
+				}
 
-				var list = data.data;
-				//				================
-				var arr = [];
-				$.each(list, function(index, data, array) {
-					var a = {
-						title: data.deadLines,
-						value: data.deadlinesId
-					}　　　
-					arr.push(a);　
-				})
-				$(".spanTime input").select({
-					title: "贷款期数",
-					items: arr
-				});
-
-			} else {
-				errAlert('提醒', '请求超时');
-			}
-		},
-		error: function(xhr, type, errorThrown) {
-			//异常处理；
-			console.log(xhr);
-			console.log(type);
+		} else {
+			$(this).removeClass('icon-danxuan').addClass('icon-danxuan2');
+			$(this).attr('box', 'false')
 		}
-	});
+	} else {
+		errAlert('提醒', '请先选择经销商')
+	}
 
 })
 
-function query(id, type) { //根据经省份ID查询产品信息
 
+function query(id, type) { //根据经省份ID查询产品信息
 	var data = {
 		provinceId: id,
 		loanType: type
 	};
-
 	$.ajax({
 		url: path + "/product/queryProductByEreaId",
 		data: JSON.stringify(data),
@@ -151,20 +111,20 @@ function query(id, type) { //根据经省份ID查询产品信息
 					})
 					console.log(arr)
 					if(type == '1'){
-								$("#Carloan").select({
-								title: "车贷分期",
-								items: arr
-							});
+						$("#Carloan").select({
+							title: "车贷分期",
+							items: arr
+						});
 					}else if(type == '2'){
 						$("#Carrisk").select({
-								title: "车险分期",
-								items: arr
-							});
+							title: "车险分期",
+							items: arr
+						});
 					}else if(type == '3'){
 						$("#Insurance").select({
-								title: "维保分期",
-								items: arr
-							});
+							title: "维保分期",
+							items: arr
+						});
 					}
 
 			} else {
@@ -198,8 +158,6 @@ $("#btn1").on("click", function() {
 		var arr = str.split("?");
 		dealersId  = arr[0];
 	}
-	
-	
 
 	
 	var loanId   = $('#Carloan').attr('data-values');    //车贷产品id
@@ -242,7 +200,7 @@ $("#btn1").on("click", function() {
 	if(arr == null || arr.length == 0){
 			errAlert("提示",'请至少选择一个产品')
 	}else{
-			localStorage.setItem('arr', JSON.stringify(arr)); //数组
+		localStorage.setItem('arr', JSON.stringify(arr)); //数组
 		localStorage.setItem('carProperty', JSON.stringify(carProperty));  //新车
 		localStorage.setItem('dealersId', JSON.stringify(dealersId));   //经销商id
 		window.location.href="apply.html"
@@ -251,4 +209,55 @@ $("#btn1").on("click", function() {
 	
 })
 
-//===============
+//==========ios兼容readonly=====
+$('input[readonly]').on('focus', function() {
+    $(this).trigger('blur');
+});
+
+//==========如果没有选择经销商============
+$('.content_div .input_span').on('click',function(){
+	if($('#distributor').val().length<1){
+		errAlert("提示",'请先选择经销商')
+		return false;
+	}
+})
+
+//=========获取贷款期数============
+function getTime(){
+	$.ajax({
+		url: path + "/deadlineCategory/query",
+		data: JSON.stringify(data),
+		dataType: "json",
+		contentType: "application/json",
+		type: "post",
+		success: function(data) {
+			if(data.code == 0) {
+				console.log(data)
+
+				var list = data.data;
+				//				================
+				var arr = [];
+				$.each(list, function(index, data, array) {
+					var a = {
+						title: data.deadLines,
+						value: data.deadlinesId
+					}　　　
+					arr.push(a);　
+				})
+				$(".spanTime input").select({
+					title: "贷款期数",
+					items: arr
+				});
+
+			} else {
+				errAlert('提醒', '请求超时');
+			}
+		},
+		error: function(xhr, type, errorThrown) {
+			//异常处理；
+			console.log(xhr);
+			console.log(type);
+		}
+	});
+
+}
