ebca81b21e9cbe702ae734daf664912126ed282f
yuqiuju0829 <451169158@qq.com>
2018-06-20 20:57:15 +0800

    紧急联系人

--- js/distributor/submission.js
+++ js/distributor/submission.js
@@ -12,27 +12,11 @@
 //  });
 //  $alert.show();
 //};
-
 $(function() {
     $(".photo").on("click",function(){
         $(this).hide();
         $(this).siblings(".up-load").show();
     })
-    // 少于12万和大于12万切换
-    $('#less12').click(function() {
-        $('#more12').removeClass('icon-danxuan').addClass('icon-danxuan2');
-        $(this).removeClass('icon-danxuan2').addClass('icon-danxuan');
-        $(".more12").hide();
-        $(".less12").show();
-    })
-    
-    $('#more12').click(function() {
-        $('#less12').removeClass('icon-danxuan').addClass('icon-danxuan2');
-        $(this).removeClass('icon-danxuan2').addClass('icon-danxuan');
-        $(".more12").show();
-        $(".less12").hide()
-    })
-
     // 允许上传的图片类型  
     var allowTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'];
     // 1024KB，也就是 1MB  
@@ -91,25 +75,19 @@ $(function() {
                     var base64 = canvas.toDataURL('image/png');
 
                     // 插入到预览区  
-
-                    // 此处新增一个关闭图标
-                    var $preview = $('<li class="weui_uploader_file weui_uploader_status" style="background-image:url(' + base64 + ')">'
-                    +'<div class="weui_uploader_status_content">0%</div><i class="iconfont icon-guanbi" id="closer"><i></li>');
+                    var $preview = $('<li class="weui_uploader_file weui_uploader_status" style="background-image:url(' + base64 + ')"><div class="weui_uploader_status_content">0%</div></li>');
                     $('.weui_uploader_files').append($preview);
                     var num = $('.weui_uploader_file').length;
                     $('.js_counter').text(num + '/' + maxCount);
 
                     // 然后假装在上传，可以post base64格式，也可以构造blob对象上传，也可以用微信JSSDK上传  
-					
+
                     var progress = 0;
 
                     function uploading() {
+                        $preview.find('.weui_uploader_status_content').text(++progress + '%');
                         if (progress < 100) {
-                        	
                             setTimeout(uploading, 30);
-                            progress = progress+1;
-                            $preview.find('.weui_uploader_status_content').text(progress);
-                            console.log($preview.find('.weui_uploader_status_content').text())
                         } else {
                             // 如果是失败，塞一个失败图标  
                             //$preview.find('.weui_uploader_status_content').html('<i class="weui_icon_warn"></i>');  
@@ -124,21 +102,119 @@ $(function() {
             reader.readAsDataURL(file);
         }
     });
-    
-    $("#closer").on("click",function() {
-        console.log("传附件大煞风景")
-        cue("提醒","是否删除？")
-        $("#yes").on("clcik",function() {
-            $(".pop-box").hide();
-            errLay("已删除")
-        })
-        $("#no").on("click",function() {
-            $(".pop-box").hide()
-        })
-    })
-
-
-    $(".weui-btn").on("click",function(){
-        window.location.href="carMsg.html"
-    })
 });
+// $(function() {
+//     $(".photo").on("click",function(){
+//         $(this).hide();
+//         $(this).siblings(".up-load").show();
+//     })
+
+//     // 允许上传的图片类型  
+//     var allowTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'];
+//     // 1024KB，也就是 1MB  
+//     var maxSize = 1024 * 1024;
+//     // 图片最大宽度  
+//     var maxWidth = 300;
+//     // 最大上传图片数量  
+//     var maxCount = 6;
+//     $('.js_file').on('change', function(event) {
+//         var files = event.target.files;
+
+//         // 如果没有选中文件，直接返回  
+//         if (files.length === 0) {
+//             return;
+//         }
+
+//         for (var i = 0, len = files.length; i < len; i++) {
+//             var file = files[i];
+//             var reader = new FileReader();
+
+//             // 如果类型不在允许的类型范围内  
+//             if (allowTypes.indexOf(file.type) === -1) {
+//                 $.weui.alert({
+//                     text: '该类型不允许上传'
+//                 });
+//                 continue;
+//             }
+
+//             if (file.size > maxSize) {
+//                 $.weui.alert({
+//                     text: '图片太大，不允许上传'
+//                 });
+//                 continue;
+//             }
+
+//             if ($('.weui_uploader_file').length >= maxCount) {
+//                 $.weui.alert({
+//                     text: '最多只能上传' + maxCount + '张图片'
+//                 });
+//                 return;
+//             }
+
+//             reader.onload = function(e) {
+//                 var img = new Image();
+//                 img.onload = function() {
+//                     // 不要超出最大宽度  
+//                     var w = Math.min(maxWidth, img.width);
+//                     // 高度按比例计算  
+//                     var h = img.height * (w / img.width);
+//                     var canvas = document.createElement('canvas');
+//                     var ctx = canvas.getContext('2d');
+//                     // 设置 canvas 的宽度和高度  
+//                     canvas.width = w;
+//                     canvas.height = h;
+//                     ctx.drawImage(img, 0, 0, w, h);
+//                     var base64 = canvas.toDataURL('image/png');
+
+//                     // 插入到预览区  
+
+//                     // 此处新增一个关闭图标
+//                     var $preview = $('<li class="weui_uploader_file weui_uploader_status" style="background-image:url(' + base64 + ')">'
+//                     +'<div class="weui_uploader_status_content">0%</div><i class="iconfont icon-guanbi" id="closer"><i></li>');
+//                     $('.weui_uploader_files').append($preview);
+//                     var num = $('.weui_uploader_file').length;
+//                     $('.js_counter').text(num + '/' + maxCount);
+
+//                     // 然后假装在上传，可以post base64格式，也可以构造blob对象上传，也可以用微信JSSDK上传  
+					
+//                     var progress = 0;
+
+//                     function uploading() {
+//                         if (progress < 100) {
+                        	
+//                             setTimeout(uploading, 30);
+//                             progress = progress+1;
+//                             $preview.find('.weui_uploader_status_content').text(progress);
+//                             console.log($preview.find('.weui_uploader_status_content').text())
+//                         } else {
+//                             // 如果是失败，塞一个失败图标  
+//                             //$preview.find('.weui_uploader_status_content').html('<i class="weui_icon_warn"></i>');  
+//                             $preview.removeClass('weui_uploader_status').find('.weui_uploader_status_content').remove();
+//                         }
+//                     }
+//                     setTimeout(uploading, 30);
+//                 };
+
+//                 img.src = e.target.result;
+//             };
+//             reader.readAsDataURL(file);
+//         }
+//     });
+    
+//     $("#closer").on("click",function() {
+//         console.log("传附件大煞风景")
+//         cue("提醒","是否删除？")
+//         $("#yes").on("clcik",function() {
+//             $(".pop-box").hide();
+//             errLay("已删除")
+//         })
+//         $("#no").on("click",function() {
+//             $(".pop-box").hide()
+//         })
+//     })
+
+
+//     $(".weui-btn").on("click",function(){
+//         window.location.href="carMsg.html"
+//     })
+// });
--- js/distributor/urgent.js
+++ js/distributor/urgent.js
@@ -1,35 +1,123 @@
 $(function(){
+    var listJson = {};
+    var importId = 1;
+    
+    getOldValue() 
+
 	var relationship = ["夫妻","父子","父女","母子","母女"];
 	
-    $("#relative").select({
-        title:"与申请人关系",
-        items:relationship
-    })
-    $(".relative1").select({
+   
+    $(".urgent .myType").select({
         title:"与申请人关系",
         items:relationship
     })
     $(".weui-btn").on("click",function(){
-        let name=$("#name").val()
-        let name1=$("#name").val()
-        let name2=$("#name").val()
-        let tel=$("#tel").val()
-        let tel1=$("#tel").val()
-        let tel2=$("#tel").val()
-        if(isName(name,"直属亲人")==false){
-            return false
-        }else if(isName(name1)==false){
-            return false
-        }else if(isName(name2)==false){
-            return false
-        }else if(isPhone(tel)==false){
-            return false
-        }else if(isPhone(tel1)==false){
-            return false
-        }else if(isPhone(tel2)==false){
-            return false
-        }else{
-            window.location.href="application.html"
+        
+        if(Verification() == false){
+            return false;
         }
+
+        var arr = [];
+        $('.urgent').each(function(){
+            let obj = {};
+            if($(this).find('.myName').val()){
+                obj.contactsName = $(this).find('.myName').val();
+            }
+            if($(this).find('.myType').val()){
+                obj.relation = $(this).find('.myType').val();
+            }
+            if($(this).find('.myPhone').val()){
+                obj.contactsWay = $(this).find('.myPhone').val();
+            }
+            arr.push(obj)
+        })
+
+        listJson.applyId = importId;
+        listJson.urgentContactList = arr;
+
+        getSave();
+            
     })
+
+    function getOldValue() {
+        let data = {
+			id: 1
+		}
+        $.ajax({
+            url: path+"/apply/getContactListByApplyId",
+            data: JSON.stringify(data),
+            dataType: "json",
+            contentType: "application/json",
+            type: "post",
+            xhrFields: {
+                withCredentials:true
+            },
+            success: function(data) {
+                if(data.code == 0){
+                    if(data.data) {
+                        listJson = data.data;
+                        for(let i = 0;i < listJson.length;i++) {
+                            if(listJson[i]){
+                                if(listJson[i].contactsName){
+                                    $('.myName').eq(i).val(listJson[i].contactsName)
+                                }
+                                if(listJson[i].relation){
+                                    $('.myType').eq(i).val(listJson[i].relation)
+                                }
+                                if(listJson[i].contactsWay){
+                                    $('.myPhone').eq(i).val(listJson[i].contactsWay)
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            error: function(xhr,type,errorThrown){
+                console.log(xhr);
+                console.log(type)
+            }
+        })
+    }
+
+    function getSave() {
+     console.log(listJson);
+        $.ajax({
+            url: path+"/apply/saveContactListByApplyIdInfo",
+            data: JSON.stringify(listJson),
+            dataType: "json",
+            contentType: "application/json",
+            type: "post",
+            traditional: false,  
+            xhrFields: {
+                withCredentials:true
+            },
+            success: function(data){
+                window.location.href = "application.html"
+            },
+            error: function(xhr,type,errorThrown){
+                console.log(xhr);
+                console.log(type)
+            }
+        })
+    }
+
+   // ==========
+    function Verification() {
+        var sta  = true;
+		$('.relative').each(function() {
+            $(this).find('input').each(function(){
+                if($(this).val() == '') {
+                    let msg = $(this).parents('.weui-cell').find('label').text();
+                    let str = msg.substr(0, msg.length - 1);
+                    errLay(str + '不能为空');
+                    sta = false
+                    return false;
+                }
+            })
+        })
+        return sta;
+    }
+    // ==========
+    
+
 })
