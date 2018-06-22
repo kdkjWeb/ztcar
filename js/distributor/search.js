dec462dcc43c5ee9f5079cb3b039beb537673a02
yuqiuju829 <451169158@qq.com>
2018-06-22 20:25:59 +0800

    信息查询的类型切换和客户姓名的模糊查询

--- js/distributor/search.js
+++ js/distributor/search.js
@@ -1,4 +1,8 @@
 $(function(){
+
+    var listJson = {};
+    var important = 1;
+
     $(".weui-btn").on("click",function(){
         let name=$("#name").val()
         let date=$("#date").val()
@@ -10,4 +14,33 @@ $(function(){
            return true
         }
     })
+
 })
+function getUserVal() {
+    let data = {
+        id = 1
+    }
+
+    $.ajax({
+        url:path,
+        data:JSON.stringify(data),
+        dataType:"json",
+        contentType:"application/json",
+        type:"post",
+        success: function(data) {
+            console.log(data);
+            if(data.code ==0) {
+                if(data.data) {
+                    listJson = data.data;
+                    if(listJson) {}
+                }
+           
+            }
+        },
+        error: function(xhr,type,errorThrown) {
+            console.log(xhr);
+            console.log(type)
+        }
+    })
+    
+}
--- js/distributor/userSearch.js
+++ js/distributor/userSearch.js
@@ -1,14 +1,51 @@
 $(function(){
-    $(".type input").select({
-        title:"选择查询类型",
-        items:["征信记录","客户姓名","汽车销价区间","汽车销售时间段"],
+
+    
+    var listJson = {};
+    var important = 1;
+
+    $(".type input").select ({
+        title: "选择查询类型",
+        items: ["征信记录","客户姓名","汽车销价区间","汽车销售时间段"],
+        onChange: function() {
+
+            if($("input[type=text]").val() == "征信记录") {
+                $(".zx-search").show().siblings(".ct").hide()
+            }
+
+            if($("input[type=text]").val() == "客户姓名") {
+                $(".user-search").show().siblings(".ct").hide()
+            }
+
+            if($("input[type=text]").val() == "汽车销价区间") {
+                $(".car-search-price").show().siblings(".ct").hide()
+            }
+
+            if($("input[type=text]").val() == "汽车销售时间段") {
+                $(".car-search").show().siblings(".ct").hide()
+            }
+        }
     });
-    $(".time input").datetimePicker({
-        times:function(){
+    $("#search").on("click",function(){
+        console.log("低分局is电话")
+        Name()
+    })
+    $(".time input").datetimePicker ({
+        times: function() {
             return
         }
     });
-    $(".look").on("click",function(){
-        window.location.href="search.html"
+    $(".look").on("click",function() {
+        window.location.href = "search.html"
     })
+    
+    function Name() {
+        var text = $("#input").val();
+        text = $.trim(text)
+        // if($.trim(text) !="") {
+            $("#table tbody tr").hide().find("td:first").filter(":contains('"+ text +"')").parent().show()
+        // }
+      
+    }
 })
+
