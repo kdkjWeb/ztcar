0a6b6895277d3eeff01b386648146ddf4e5114aa
yuqiuju829 <451169158@qq.com>
2018-06-28 23:35:29 +0800

    身价测量仪

--- js/distributor/application.js
+++ js/distributor/application.js
@@ -1,9 +1,13 @@
 $(function(){
+    var listJson = {}
+    var importId = GetRequest().applyId;
     $(".send").on("click",function(){
+        Create()
         cue("提醒","是否发送至经销商邮箱？")
         $("#yes").on("click",function(){
-            $(".pop-box").hide()
-            errLay("已发送至经销商邮箱")
+            Send()
+            // $(".pop-box").hide()
+            // errLay("已发送至经销商邮箱")
         })
         $("#no").on("click",function(){
             $(".pop-box").hide()
@@ -12,4 +16,47 @@ $(function(){
     $(".weui-btn").on("click",function(){
         window.location.href="submission.html"
     })
+
+    function Create() {
+        var data = {
+            applyId:importId
+        }
+        $.ajax({
+            url:path+"/compact/buildCompact",
+            data:data,
+            dataType:"json",
+            contentType:"application/json",
+            type:"get",
+            xhrFields:{
+                withCredentials: true
+            },
+            success:function(data){
+                if(data.code == 0){
+                  
+                    
+                }else {
+					errLay(data.msg)
+				}
+            }
+        })
+    }
+
+    function Send() {
+
+        $.ajax({
+            url: path + "/compact/sendCompact",
+            data:JSON.stringify(listJson),
+            dataType:"json",
+            contentType:"application/json",
+            type:"post",
+            success: function(data) {
+                console.log(data)
+
+            },
+            error: function(xhr,type,errthrown) {
+                console.log(xhr);
+                console.log(type)
+            }
+        })
+    }
 })
--- js/public/income.js
+++ js/public/income.js
@@ -1,19 +1,18 @@
 $(function(){
     $(document).on("click","li",function(){
         window.location.href="honor.html"
+        move()
     })
     $(document).on("click",".comeBack",function() {
-        console.log("但是")
-        // window.location.reload()
         window.location.href = "occupation.html"
     })
     let arr=[
-        {price:"2000及以下"},
-        {price:"2001-5000"},
-        {price:"5001-8000"},
-        {price:"8001-10000"},
-        {price:"10001-12000"},
-        {price:"12000以上"}
+        {price:"20000以上"},
+        {price:"12000-20000"},
+        {price:"8000-12000"},
+        {price:"5000-8000"},
+        {price:"3000-5000"},
+        {price:"3000以下"}
     ]
     for(let i = 0 ;i < arr.length ; i++) {
         $("ul").append(`<li>
@@ -24,6 +23,5 @@ $(function(){
         </li>`)
         console.log(`${arr[i].price}`)
     }
-
-
+   
 })
